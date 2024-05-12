202a5792dc7feb258f27b51fc970b86becf29098
Sasha Denouvilliez-Pech <sasha.denouvilliez-pech@mail.mcgill.ca>
2024-05-12 14:57:29 -0400

    fix typos

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,7 +3,7 @@ import { User } from "@prisma/client";
 
 
 /**
- * Finds uer by email
+ * Finds user by email
  */
 export async function findUserByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
@@ -15,20 +15,19 @@ export async function findUserByEmail(email: string): Promise<User | null> {
 }
 
 export async function findUserById(id: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
+    return await prisma.user.findUnique({
         where: {
             id
         }
     });
-    return user;
 }
 
 export async function findAllUsers(): Promise<User[]> {
     return await prisma.user.findMany();
 }
 
-export async function deleteUserById(id: string): Promise<void> {
-    await prisma.user.delete({
+export async function deleteUserById(id: string): Promise<User | null> {
+    return await prisma.user.delete({
         where: {
             id
         }
