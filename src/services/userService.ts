988bdf9e1c8e82c76ae9c526b193dfbe14b8171f
Sasha Denouvilliez-Pech <97471446+sashadp01@users.noreply.github.com>
2024-05-16 09:07:04 -0400

    feat: Delete and get user (#43)

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,7 +3,7 @@ import { User } from "@prisma/client";
 
 
 /**
- * Finds uer by email
+ * Finds user by email
  */
 export async function findUserByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
@@ -12,4 +12,24 @@ export async function findUserByEmail(email: string): Promise<User | null> {
         }
     });
     return user;
+}
+
+export async function findUserById(id: string): Promise<User | null> {
+    return await prisma.user.findUnique({
+        where: {
+            id
+        }
+    });
+}
+
+export async function findAllUsers(): Promise<User[]> {
+    return await prisma.user.findMany();
+}
+
+export async function deleteUserById(id: string): Promise<User | null> {
+    return await prisma.user.delete({
+        where: {
+            id
+        }
+    });
 }
