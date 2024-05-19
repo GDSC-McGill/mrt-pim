488df33385168fcdde8d2efd08ff9caa75f2412f
massderrico <massderrico@yahoo.com>
2024-05-19 11:57:15 -0400

    syntax and naming fixes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,21 +1,22 @@
-import { findUserByEmail, findUserById, findAllUsers, deleteUserById } from "@/services/userService";
+import { User } from "@prisma/client";
+import { findUserByEmail, findUserById, findAllUsers, deleteUserById} from "@/services/userService";
 
-export async function findUserByEmail(email: string): Promise<User | null> {
+export async function getUserByEmail(email: string): Promise<User | null> {
   const user = await findUserByEmail(email);
   return user;
 }
 
-export async function findUserById(id: string): Promise<User | null> {
-  const user await findUserById(id);
+export async function getUserById(id: string): Promise<User | null> {
+  const user = await findUserById(id);
   return user;
 }
 
-export async function findAllUsers(): Promise<User[] | null> {
+export async function getAllUsers(): Promise<User[] | null> {
   const users = await findAllUsers();
   return users;
 }
 
-export async function deleteUserById(id: string): Promise<User | null> {
+export async function removeUserById(id: string): Promise<User | null> {
   const deletedUser = deleteUserById(id);
   return deletedUser;
 }
