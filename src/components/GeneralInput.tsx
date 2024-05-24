527ff80fb022014f7ce34f221be40a1effca3207
Arman Ahmed <arman.ahmed@mail.mcgill.ca>
2024-05-24 04:48:58 -0400

    fixed redundancy with booleans, added optional flag to labels, rebased the branch, removed unneeded import statements.

--- src/components/GeneralInput.tsx
+++ src/components/GeneralInput.tsx
@@ -1,24 +1,18 @@
-import { CloudMoonRain } from 'lucide-react';
 import React from 'react';
 import { Input } from './lib/ui/input';
-import { Trykker } from 'next/font/google';
 
 type inputProps = {
-    primaryTextVisible: boolean,
-    secondaryTextVisible: boolean,
-    primaryText: string,
-    secondaryText: string,
+    primaryText?: string,
+    secondaryText?: string,
 }
 const GeneralInput = (props: inputProps) => {
-    let primaryVisible = props.primaryTextVisible ? props.primaryTextVisible : true;
-    let secondaryVisible = props.secondaryTextVisible ? props.secondaryTextVisible : true;
     let primaryText = props.primaryText;
     let secondaryText = props.secondaryText;
     return (
         <div>
-            {primaryVisible && <h1 className="text-primary font-medium" style={{paddingBottom: 10}}>{primaryText}</h1>}
-            {secondaryVisible && <Input placeholder={secondaryText}/>}
-            {!secondaryVisible && <Input />}
+            {primaryText && <h1 className="text-primary font-medium" style={{paddingBottom: 10}}>{primaryText}</h1>}
+            {secondaryText && <Input placeholder={secondaryText}/>}
+            {!secondaryText && <Input />}
         </div>
     );
 };
